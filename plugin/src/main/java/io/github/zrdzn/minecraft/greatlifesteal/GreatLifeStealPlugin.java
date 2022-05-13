99bf48e50c91db66b3f4bacbd55409b4b2784cf5
zrdzn <zrdzn9@gmail.com>
2022-05-13 16:30:42 +0200

    Initial commit

---
+++ plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/GreatLifeStealPlugin.java
@@ -0,0 +1,12 @@
+package io.github.zrdzn.minecraft.greatlifesteal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GreatLifeStealPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+}
