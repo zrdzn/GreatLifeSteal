48d3f39e3b15c287227a8c8b610a18b139efb51d
zrdzn <zrdzn9@gmail.com>
2022-05-14 20:51:32 +0200

    Optimize imports

--- plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/user/UserListener.java
+++ plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/user/UserListener.java
@@ -1,6 +1,5 @@
 package io.github.zrdzn.minecraft.greatlifesteal.user;
 
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -9,7 +8,6 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 public class UserListener implements Listener {
 
