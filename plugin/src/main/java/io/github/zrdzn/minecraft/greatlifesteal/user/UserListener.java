f8db80b6e45b24e02a417c53cc7a0fe0ff06782f
some0ne3 <53973517+some0ne3@users.noreply.github.com>
2022-05-18 23:46:39 +0200

    Handle player death, adjust player's, killer's maxHealth

--- plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/user/UserListener.java
+++ plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/user/UserListener.java
@@ -23,14 +23,43 @@ public class UserListener implements Listener {
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player player = event.getEntity();
 
+        if (player.getKiller() == null) {
+            return;
+        }
+
         UUID playerUuid = player.getUniqueId();
+
+        player.setMaxHealth(player.getMaxHealth() - this.healthChange);
 
         if (this.userService.createUser(playerUuid, (int) (player.getMaxHealth() - this.healthChange)).join()) {
             return;
         }
 
         if (!this.userService.changeHealth(playerUuid, -this.healthChange).join()) {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "Wystąpił błąd podczas nadawania serc."));
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cWystąpił błąd podczas odejmowania serc."));
         }
+
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPlayerKill(PlayerDeathEvent event) {
+        Player killer = event.getEntity().getKiller();
+
+        if (killer == null) {
+            return;
+        }
+
+        UUID killerUuid = killer.getUniqueId();
+
+        killer.setMaxHealth(killer.getMaxHealth() + this.healthChange);
+
+        if (this.userService.createUser(killerUuid, (int) (killer.getMaxHealth() + this.healthChange)).join()) {
+            return;
+        }
+
+        if (!this.userService.changeHealth(killerUuid, +this.healthChange).join()) {
+            killer.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cWystąpił błąd podczas dodawania serc."));
+        }
+
     }
 }
