692cb9aecdf87945e2f737e00b403266d55cfc2c
zrdzn <zrdzn9@gmail.com>
2022-05-19 01:02:32 +0200

    Optimize import

--- plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/user/UserListener.java
+++ plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/user/UserListener.java
@@ -3,7 +3,6 @@ package io.github.zrdzn.minecraft.greatlifesteal.user;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
