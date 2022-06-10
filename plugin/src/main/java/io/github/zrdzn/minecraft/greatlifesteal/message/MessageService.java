b91c153bf9f2491e1e399785010245012182ec25
zrdzn <zrdzn9@gmail.com>
2022-06-10 21:25:38 +0200

    Add unit test for MessageService

--- plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/message/MessageService.java
+++ plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/message/MessageService.java
@@ -1,7 +1,7 @@
 package io.github.zrdzn.minecraft.greatlifesteal.message;
 
 import io.github.zrdzn.minecraft.greatlifesteal.GreatLifeStealPlugin;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.command.CommandSender;
 
 import java.util.concurrent.CompletableFuture;
@@ -10,19 +10,21 @@ public class MessageService {
 
     public static CompletableFuture<Void> send(CommandSender receiver, final String message, String... placeholders) {
         return CompletableFuture.runAsync(() -> {
-            int length = placeholders.length;
-            if (length <= 0 || length % 2 != 0) {
-                receiver.sendMessage(GreatLifeStealPlugin.formatColor(message));
-                return;
-            }
-
-            String formattedMessage = message;
-            for (int index = 0; index < length; index += 2) {
-                formattedMessage = StringUtils.replace(formattedMessage, placeholders[index], placeholders[index + 1]);
-            }
-
-            receiver.sendMessage(GreatLifeStealPlugin.formatColor(formattedMessage));
+            receiver.sendMessage(GreatLifeStealPlugin.formatColor(formatPlaceholders(message, placeholders)));
         });
+    }
+
+    public static String formatPlaceholders(String message, String... placeholders) {
+        int length = placeholders.length;
+        if (length <= 0 || length % 2 != 0) {
+            return message;
+        }
+
+        for (int index = 0; index < length; index += 2) {
+            message = StringUtils.replace(message, placeholders[index], placeholders[index + 1]);
+        }
+
+        return message;
     }
 
 }
