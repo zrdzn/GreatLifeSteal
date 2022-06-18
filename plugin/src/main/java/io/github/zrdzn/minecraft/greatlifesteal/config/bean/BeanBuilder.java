35fa1f627e443194bc6941fbc100f8ca3602f9bb
zrdzn <zrdzn9@gmail.com>
2022-06-18 17:40:14 +0200

    Add BeanBuilder java doc comments

--- plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/config/bean/BeanBuilder.java
+++ plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/config/bean/BeanBuilder.java
@@ -6,7 +6,7 @@ public class BeanBuilder<T> {
 
     private T bean;
 
-    public BeanBuilder(Class<T> bean) {
+    private BeanBuilder(Class<T> bean) {
         try {
             this.bean = bean.newInstance();
         } catch (InstantiationException | IllegalAccessException exception) {
@@ -14,15 +14,35 @@ public class BeanBuilder<T> {
         }
     }
 
+    /**
+     * Define what is the bean class you want to work on.
+     *
+     * @param bean the class of the bean
+     * @param <T> the bean object
+     *
+     * @return the new builder holding the bean instance
+     */
     public static <T> BeanBuilder<T> from(Class<T> bean) {
         return new BeanBuilder<>(bean);
     }
 
+    /**
+     * Modify the bean and return the builder with it.
+     *
+     * @param modify the consumer for the bean
+     *
+     * @return the builder with the modified bean
+     */
     public BeanBuilder<T> with(Consumer<T> modify) {
         modify.accept(this.bean);
         return this;
     }
 
+    /**
+     * Gets the final bean.
+     *
+     * @return the bean
+     */
     public T build() {
         return this.bean;
     }
