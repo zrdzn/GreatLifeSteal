c26bdd584cd097870f9c31cbe1a4cc947fff5389
zrdzn <zrdzn9@gmail.com>
2022-06-21 17:17:59 +0200

    Add missing comments

--- plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/config/configs/HealthChangeConfig.java
+++ plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/config/configs/HealthChangeConfig.java
@@ -1,5 +1,6 @@
 package io.github.zrdzn.minecraft.greatlifesteal.config.configs;
 
+import ch.jalu.configme.Comment;
 import ch.jalu.configme.SettingsHolder;
 import ch.jalu.configme.configurationdata.CommentsConfiguration;
 import ch.jalu.configme.properties.IntegerProperty;
@@ -10,11 +11,13 @@ import ch.jalu.configme.properties.Property;
  */
 public class HealthChangeConfig implements SettingsHolder {
 
+    @Comment("How much health points should be taken from a victim.")
     public static final Property<Integer> VICTIM = new IntegerProperty(
             "baseSettings.healthChange.victim",
             2
     );
 
+    @Comment("How much health points should be given to a killer.")
     public static final Property<Integer> KILLER = new IntegerProperty(
             "baseSettings.healthChange.killer",
             2
