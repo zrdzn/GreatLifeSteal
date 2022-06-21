6e61e64d5e29b85d1e28f3628eff1f6e6e8491c5
zrdzn <87321869+zrdzn@users.noreply.github.com>
2022-06-21 22:45:51 +0200

    GH-89 Replace old take/give keys with health change set to 0  (#96)
    * GH-89 Replace old take/give keys with health change set to 0

--- plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/config/ConfigMigrationService.java
+++ plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/config/ConfigMigrationService.java
@@ -32,7 +32,8 @@ public class ConfigMigrationService extends PlainMigrationService {
     protected boolean performMigrations(PropertyReader reader, ConfigurationData configData) {
         return migrateCraftingRecipeToCrafting(reader, configData) |
                 migrateEliminationModeToCustomActions(reader, configData) |
-                migratesHealthChangeToHealthChangeSection(reader, configData) ||
+                migrateHealthChangeToHealthChangeSection(reader, configData) |
+                removeTakeGiveHeartsFromPlayers(reader, configData) ||
                 hasDeprecatedKeys(reader);
     }
 
@@ -141,7 +142,7 @@ public class ConfigMigrationService extends PlainMigrationService {
      *
      * @return the state whether migration is required
      */
-    private static boolean migratesHealthChangeToHealthChangeSection(PropertyReader reader, ConfigurationData configData) {
+    private static boolean migrateHealthChangeToHealthChangeSection(PropertyReader reader, ConfigurationData configData) {
         String oldKey = "baseSettings.healthChange";
         PropertyValue<Integer> healthProperty = new IntegerProperty(oldKey, 0).determineValue(reader);
         if (!healthProperty.isValidInResource()) {
@@ -156,12 +157,47 @@ public class ConfigMigrationService extends PlainMigrationService {
         return MIGRATION_REQUIRED;
     }
 
+    /**
+     * Removes the old take/give hearts from a victim/killer and
+     * replaces it with their healthChange set to 0.
+     *
+     * @since 1.6
+     *
+     * @param reader the config reader
+     * @param configData the config inmemory
+     *
+     * @return the state whether migration is required
+     */
+    private static boolean removeTakeGiveHeartsFromPlayers(PropertyReader reader, ConfigurationData configData) {
+        String oldVictimKey = "baseSettings.takeHealthFromVictim";
+        String oldKillerKey = "baseSettings.giveHealthToKiller";
+
+        PropertyValue<Boolean> oldVictimProperty = new BooleanProperty(oldVictimKey, true).determineValue(reader);
+        PropertyValue<Boolean> oldKillerProperty = new BooleanProperty(oldKillerKey, true).determineValue(reader);
+
+        if (!oldVictimProperty.isValidInResource() && !oldKillerProperty.isValidInResource()) {
+            return NO_MIGRATION_NEEDED;
+        }
+
+        if (oldVictimProperty.getValue()) {
+            configData.setValue(HealthChangeConfig.VICTIM, 0);
+        }
+
+        if (oldKillerProperty.getValue()) {
+            configData.setValue(HealthChangeConfig.KILLER, 0);
+        }
+
+        return MIGRATION_REQUIRED;
+    }
+
     private static boolean hasDeprecatedKeys(PropertyReader reader) {
         List<String> deprecatedKeys = new ArrayList<String>() {
             {
                 this.add("baseSettings.heartItem.craftingRecipe");
                 this.add("baseSettings.eliminationMode");
                 this.add("baseSettings.healthChange");
+                this.add("baseSettings.takeHealthFromVictim");
+                this.add("baseSettings.giveHealthToKiller");
             }
         };
 
