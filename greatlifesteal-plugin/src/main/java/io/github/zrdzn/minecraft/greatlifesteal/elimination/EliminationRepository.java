4fdf27357049e623240717b1502327b1db076526
zrdzn <zrdzn9@gmail.com>
2022-08-07 19:17:23 +0200

    Add JavaDoc

--- greatlifesteal-plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/elimination/EliminationRepository.java
+++ greatlifesteal-plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/elimination/EliminationRepository.java
@@ -8,16 +8,51 @@ import panda.std.Result;
 
 public interface EliminationRepository {
 
+    /**
+     * Saves a new record in the database.
+     *
+     * @param elimination the elimination dto
+     * @return the newly created record with an optional exception
+     */
     Result<Elimination, Exception> save(Elimination elimination);
 
+    /**
+     * Lists all existing records from the database.
+     *
+     * @return all existing records with an optional exception
+     */
     Result<List<Elimination>, Exception> listAll();
 
+    /**
+     * Finds a record with the specified id.
+     *
+     * @param id the id of a record
+     * @return an optional elimination with an optional exception
+     */
     Result<Optional<Elimination>, Exception> findById(int id);
 
+    /**
+     * Finds a record with the specified player's uuid.
+     *
+     * @param playerUuid the uuid of a player
+     * @return an optional elimination with an optional exception
+     */
     Result<Optional<Elimination>, Exception> findByPlayerUuid(UUID playerUuid);
 
+    /**
+     * Deletes a record with the specified id.
+     *
+     * @param id the id of a record
+     * @return an optional exception
+     */
     Result<Blank, Exception> deleteById(int id);
 
+    /**
+     * Deletes a record with the specified player's uuid.
+     *
+     * @param playerUuid the uuid of a player
+     * @return an optional exception
+     */
     Result<Blank, Exception> deleteByPlayerUuid(UUID playerUuid);
 
 }
