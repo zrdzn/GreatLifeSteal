acf549f79bfdba0d7c28f9da6b736ccdaa6bdd21
zrdzn <zrdzn9@gmail.com>
2025-05-28 13:09:03 +0200

    GH-237 Add support for 1.20.x and 1.21.x (Closes #237)

--- greatlifesteal-plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/SpigotServerFactory.java
+++ greatlifesteal-plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/SpigotServerFactory.java
@@ -20,25 +20,40 @@ public class SpigotServerFactory {
     }
 
     public SpigotServer createServer() {
-        String version = this.plugin.getServer().getClass().getPackage().getName().split("\\.")[3];
-        switch (version) {
-            case "v1_8_R3":
-                return new V1_8R3SpigotServer();
-            case "v1_9_R2":
-                return new V1_9R2SpigotServer();
-            case "v1_10_R1":
-                return new V1_10R1SpigotServer();
-            case "v1_11_R1":
-                return new V1_11R1SpigotServer();
-            case "v1_12_R1":
-                return new V1_12R1SpigotServer();
-            case "v1_13_R2":
-                return new V1_13R2SpigotServer();
-            case "v1_14_R1":
-                return new V1_14R1SpigotServer(this.plugin);
-            default:
-                return new V1_15R1SpigotServer(this.plugin);
+        SpigotServer spigotServer;
+        try {
+            String version = this.plugin.getServer().getClass().getPackage().getName().split("\\.")[3];
+
+            switch (version) {
+                case "v1_8_R3":
+                    spigotServer = new V1_8R3SpigotServer();
+                    break;
+                case "v1_9_R2":
+                    spigotServer = new V1_9R2SpigotServer();
+                    break;
+                case "v1_10_R1":
+                    spigotServer = new V1_10R1SpigotServer();
+                    break;
+                case "v1_11_R1":
+                    spigotServer = new V1_11R1SpigotServer();
+                    break;
+                case "v1_12_R1":
+                    spigotServer = new V1_12R1SpigotServer();
+                    break;
+                case "v1_13_R2":
+                    spigotServer = new V1_13R2SpigotServer();
+                    break;
+                case "v1_14_R1":
+                    spigotServer = new V1_14R1SpigotServer(this.plugin);
+                    break;
+                default:
+                    spigotServer = new V1_15R1SpigotServer(this.plugin);
+            }
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            spigotServer = new V1_15R1SpigotServer(this.plugin);
         }
+
+        return spigotServer;
     }
 
 }
