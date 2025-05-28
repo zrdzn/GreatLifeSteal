40f8c6fee75b3acf93245c3475b2e6e4fa3a6d22
zrdzn <zrdzn9@gmail.com>
2025-05-28 13:21:32 +0200

    Remove unnecessary logger info

--- greatlifesteal-plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/heart/HeartItemStackFactory.java
+++ greatlifesteal-plugin/src/main/java/io/github/zrdzn/minecraft/greatlifesteal/heart/HeartItemStackFactory.java
@@ -6,14 +6,10 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static io.github.zrdzn.minecraft.greatlifesteal.GreatLifeStealPlugin.formatColor;
 
 public class HeartItemStackFactory {
-
-    private final Logger logger = LoggerFactory.getLogger(HeartItemStackFactory.class);
 
     private final HeartConfig config;
     private final SpigotServer spigotServer;
@@ -28,9 +24,7 @@ public class HeartItemStackFactory {
 
         NbtService nbtService = this.spigotServer.getNbtService();
 
-        this.logger.info("Applying '{}' nbt key to heart item.", HeartItem.HEART_NBT_KEY);
         nbtService.setBoolean(itemStack, HeartItem.HEART_NBT_KEY, true);
-        this.logger.info("Applying '{}' nbt key to heart item.", HeartItem.HEART_HEALTH_CHANGE_NBT_KEY);
         nbtService.setDouble(itemStack, HeartItem.HEART_HEALTH_CHANGE_NBT_KEY, this.config.getHealthAmount());
 
         ItemMeta heartItemMeta = itemStack.getItemMeta();
